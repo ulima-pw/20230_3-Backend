d76f095dd6d9f36707099c49fe3e4c513bf1c1b0
ProfL3403 <ProfL3403@lab.aca>
2023-02-06 18:33:52 -0500

    trabajando endpoint post

--- prueba/urls.py
+++ prueba/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("hola", views.holaEndpoint),
-    path("adios", views.endpoint2)
+    path("adios", views.endpoint2),
+    path("html", views.htmlEndpoint),
+    path("post", views.postFormularioEndpoint)
 ]
--- prueba/views.py
+++ prueba/views.py
@@ -1,5 +1,6 @@
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 # Ruta: /hola
@@ -10,3 +11,32 @@ def holaEndpoint(request):
 # Ruta: /adios
 def endpoint2(request):
     return HttpResponse("ADIOS!!!") 
+
+# Ruta: /html
+def htmlEndpoint(request):
+    return HttpResponse("""
+        <html>
+            <body>
+                <h1>Programacion Web</h1>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Universidad_de_Lima_logo.png" />
+                <form method="post" action="/prueba/post">
+                    <input type="text" name="username" />
+                    <input type="password" name="password" />
+                    <button type="submit">Enviar</button>
+                </form>
+            </body>
+        </html>
+    """)
+
+# Ruta: /post
+@csrf_exempt
+def postFormularioEndpoint(request):
+    if request.method == "POST":
+        usuario = request.POST.get("username")
+        password = request.POST.get("password")
+        return HttpResponse(f"""
+            <p>Usuario: {usuario}</p>
+            <p>Password: {password}</p>
+        """)
+
+    return HttpResponse("Llego")
